b5bb30173a5eed1685b331a97fcaf4a173fa894e
Alyssa Easter <100172781+alyssa43@users.noreply.github.com>
2024-07-05 13:56:29 -0700

    Worked on mental model

--- mental_model.rb
+++ mental_model.rb
@@ -35,3 +35,55 @@
 
 # Go over quizzes and re-format questions to re-quiz yourself
 
+class Person
+  attr_reader :name
+
+  def initialize(name, age, dob)
+    @name = name
+    @age = age
+    @dob = dob
+  end
+
+  def older_than?(other_person)
+    age > other_person.age
+  end
+
+  def birthyear
+    dob.strftime('%Y')
+  end
+
+  protected
+
+  attr_reader :age
+
+  private
+
+  attr_reader :dob
+end
+
+class Car
+  def initialize(make, model, year)
+    @make = make
+    @model = model
+    @year = year
+  end
+
+  protected
+
+  def age
+    Time.now.year - @year
+  end
+end
+
+bob = Person.new('Robert', 24, Time.new(2000, 1, 15))
+joe = Person.new('Joesph', 40, Time.new(1984, 3, 1))
+
+car = Car.new('Chevy', 'Suburban', 2015)
+
+p bob.name # => "Robert"
+# p bob.age # => NoMethodError because `age` is protected it cannot be referenced outside of class
+# p bob.dob # => NoMethodError because `dob` is private it cannot be referenced outside of class
+p bob.older_than?(joe) # => false -> because both `bob` and `joe` are instances of the `Person` class we can invoke both objects `age` getter methods from within the `Person` class even though the `age` getter methods are protected. This only works because they are instances of the same class and the protected method invocation is being called within the same class. If we tried to compare the age of somehting else, say a car that also has a protected `age` instance method we would get a NoMethodError exception:
+# p joe.older_than?(car) # => NoMethodError because the Car class' `age` instance method is protected
+
+p bob.birthyear # => "2000"
